ac44f6a6ff5630159c6763c53285f177129cd0fe
YoganandhanTK <k.thangamani731@gmail.com>
2025-03-06 13:31:32 +0530

    Fitflex-Project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero-container' id='hero'>
+      
+      <div className="hero-text">
+        <span>
+          <div className="hero-line" />
+          <h5>FitFlex</h5>
+        </span>
+        <h2><b className='txt-color1'>FitFlex Chronicles: </b>
+        <b> Bend with Power
+        </b> Burn with Purpose Become 
+        <b className='txt-color2'> Legendary !</b></h2>
+        
+
+
+        <a href="#search"><button>View more</button></a>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
